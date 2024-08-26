625f0d3de774826505d8e15510af01b4a171fcca
Ubuntu <ubuntu@ip-172-31-8-141.ap-south-1.compute.internal>
2024-08-26 11:38:36 +0000

    similar changes

--- calculator.sh
+++ calculator.sh
@@ -1,9 +1,15 @@
 #!bin/bash
 #Addition
+#this is small change
 #x=a+b
 #subraction is added
 #y=a-b
 #Division is added
 #z=a/b
+<<<<<<< HEAD
 #exoponentgial is added
 #y1=a^b
+=======
+#Mulitiplication is added
+#x1=a*b
+>>>>>>> 40a72da (a small change)
